dc0373cb6c5de41d3cf382a3d2c31cdfa55d86bb
nothellena <hac@ic.ufal.br>
2022-11-28 00:21:47 -0300

    Integração com a API

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,35 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Checklist from "./components/Checklist";
+import api from "./services/api";
 import "./styles.css";
 
 export default function App() {
   const [input, setInput] = useState("");
   const [lista, setLista] = useState([]);
-  const [id, setId] = useState(0)
+  const [id_tarefa, setId_tarefa] = useState(null);
+
+  useEffect(() => {
+    api
+      .get("/tarefas")
+      .then((res) => {
+        setLista(JSON.parse(res.data.replace(/'/g, '"')));
+      })
+      .catch((erro) => console.log(erro.message));
+  }, []);
 
   const handleInput = (e) => {
     setInput(e.target.value);
   };
 
   const handleAdicionarClick = () => {
-    if(input.length > 0) {
-      setLista([...lista, { id: id, titulo: input }]);
-      setInput("");
-      setId(id+1);
+
+    if (input.length > 0) {
+      const data = new FormData();
+      data.append("id_tarefa", id_tarefa);
+      data.append("titulo", input);
+      data.append("status", 0);
+
+      api.post("/tarefas/cadastrar", data)
+        .then((res) => {
+          setLista([...lista, { id_tarefa: id_tarefa, titulo: input }]);
+          setInput("");
+          setId_tarefa(id_tarefa + 1);
+
+          console.log(res);
+        })
+        .catch((erro) => console.log(erro.message));
     }
   };
+
   const handleAdicionarEnter = (e) => {
     if (e.key === "Enter" && input.length > 0) {
-      setLista([...lista, { id: id, titulo: input }]);
-      setInput("");
-      setId(id+1)
+      const data = new FormData();
+      data.append("id_tarefa", id_tarefa);
+      data.append("titulo", input);
+      data.append("status", 0);
+
+      api.post("/tarefas/cadastrar", data)
+        .then((res) => {
+          setLista([...lista, { id_tarefa: id_tarefa, titulo: input }]);
+          setInput("");
+          setId_tarefa(id_tarefa + 1);
+
+          console.log(res);
+        })
+        .catch((erro) => console.log(erro.message));
     }
   };
 
-  const handleExcluir = (id) => {
-    const remove = [...lista].filter(todo => todo.id !== id)
-
-    setLista(remove)
+  const handleExcluir = (id_tarefa) => {
+    api
+      .delete(`/tarefas/${id_tarefa}/deletar`)
+      .then((res) => {
+        console.log(res);
+        const remove = [...lista].filter(
+          (todo) => todo.id_tarefa !== id_tarefa
+        );
+        setLista(remove);
+      })
+      .catch((erro) => console.log(erro.message));
   };
 
   return (
@@ -54,13 +95,20 @@ export default function App() {
             src="assets/botaoAdicionar.svg"
             alt="Adicionar afazer"
             onClick={handleAdicionarClick}
-           
           />
         </div>
         <div className="lista">
-          { lista.length > 0? lista.map(li => (
-            <Checklist onHandleExcluir={() => handleExcluir(li.id)} key={li.id} input={li.titulo} />
-          )): <h4>SEM TAREFAS!</h4>}
+          {lista.length > 0 ? (
+            lista.map((li) => (
+              <Checklist
+                onHandleExcluir={() => handleExcluir(li.id_tarefa)}
+                id={li.id_tarefa}
+                input={li.titulo}
+              />
+            ))
+          ) : (
+            <h4>SEM TAREFAS!</h4>
+          )}
         </div>
       </div>
     </div>
--- todo-app/src/components/Checklist/index.js
+++ todo-app/src/components/Checklist/index.js
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
+import api from "../../services/api";
 import "./styles.css";
-
-
 
 export default function Check(props) {
   const [check, setCheck] = useState(false);
 
   const handleCheck = () => {
-    setCheck(!check)
-  }
+    const data = new FormData();
+    data.append("status", check ? 0 : 1);
 
+    api.put(`/tarefas/${props.id}/atualizar`, data)
+      .then((res) => {
+        setCheck(!check);
+        console.log(res);
+      })
+      .catch((erro) => console.log(erro.message));
+  };
 
   return (
     <div className="check">
-      <label key={props.key}>
+      <label key={props.id}>
         <input type="checkbox" checked={check} onClick={handleCheck} />
         <span>{props.input}</span>
       </label>
-      <img src="/assets/lixeira.svg" alt="excluir" onClick={props.onHandleExcluir} />
+      <img
+        src="/assets/lixeira.svg"
+        alt="excluir"
+        onClick={props.onHandleExcluir}
+      />
     </div>
   );
 }
-
